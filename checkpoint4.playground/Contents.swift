68430287752bd9a139d899b30d0fa35b1f498475
celuu <christine.e.luu@gmail.com>
2023-04-06 21:22:11 -0700

    Working on checkpoint 4

--- checkpoint4.playground/Contents.swift
+++ checkpoint4.playground/Contents.swift
@@ -1,3 +1,14 @@
 import UIKit
 
-var greeting = "Hello, playground"
+enum numError: Error {
+    case outOfBounds:
+        return "It is out of bounds"
+}
+
+
+func squareRoot (num: Int) throws {
+    if num < 1 && num > 10000 {
+        numError.outofBounds
+    }
+    
+}
